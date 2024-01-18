267b198d1a16b6b47d7f302e4e4abf7609854b88
f4cknet <zmzsg100@gmail.com>
2024-01-18 23:09:45 +0800

    delete

--- app/config.py
+++ app/config.py
@@ -1,2 +1,12 @@
-ossak = 'LTAI5tEyyE9DQEuvttYGXfVM'
-osssk = 'WScQ8FulUi7jHNDr9DYqMrOFTPqEJr'
+
+ossak = ''
+osssk = ''
+
+SQLALCHEMY_DATABASE_URI = "mysql://hackme:Hackme123!@localhost:3306/hackme?charset=utf8"
+SECRET_KEY = "hackmemdakldmsakl"
+MAIL_SERVER = 'smtp.163.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_DEFAULT_SENDER = ''
--- test/1.py
+++
@@ -1,8 +0,0 @@
-import mmh3
-import requests
-import base64
-
-response = requests.get('https://bo.aichatme.com/favicon.ico')
-favicon = base64.b64encode(response.content)
-hash = mmh3.hash(favicon)
-print('http.favicon.hash:'+str(hash))
